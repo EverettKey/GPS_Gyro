9319230e2cd07611100670c148205000b78c4f2f
EverettKey <everett.k.key@gmail.com>
2017-08-09 01:56:30 -0700

    Both GPS and Gyro seems to be working, check for errors later

--- GPS_Shield.ino
+++ GPS_Shield.ino
@@ -37,17 +37,18 @@ int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 // Keep in mind, the SD library has max file name lengths of 8.3 - 8 char prefix,
 // and a 3 char suffix.
 // Our log files are called "gpslogXX.csv, so "gpslog99.csv" is our max file.
-#define LOG_FILE_PREFIX F("gpslog") // Name of the log file.
+#define LOG_FILE_PREFIX "gpslog" // Name of the log file.
 #define MAX_LOG_FILES 100 // Number of log files that can be made
-#define LOG_FILE_SUFFIX F("csv") // Suffix of the log file
+#define LOG_FILE_SUFFIX "csv" // Suffix of the log file
 #define comma F(",")
 char logFileName[13]; // Char string to store the log file name
 // Data to be logged:
-#define LOG_COLUMN_COUNT 15
-char * log_col_names[LOG_COLUMN_COUNT] = {
-  F("longitude"), F("latitude"), F("altitude"), F("speed"), F("course"), F("date"), F("time"), F("satellites"),
-  F("AcX"), F("AcY"), F("AcZ"), F("Tmp"), F("GyX"), F("GyY"), F("GyZ")
-}; // log_col_names is printed at the top of the file.
+//#define LOG_COLUMN_COUNT 15
+#define COLUMN_NAMES F("longitude, latitude, altitude, speed, course, date, time, satellites, AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ")
+//char * log_col_names[LOG_COLUMN_COUNT] = {
+//  "longitude", "latitude", "altitude", "speed", "course", "date", "time", "satellites",
+//  "AcX", "AcY", "AcZ", "Tmp", "GyX", "GyY", "GyZ"
+//}; // log_col_names is printed at the top of the file.
 
 //////////////////////
 // Log Rate Control //
@@ -90,7 +91,11 @@ void setup()
     SerialMonitor.println(F("Error initializing SD card."));
   }
   updateFileName(); // Each time we start, create a new file, increment the number
-  printHeader(); // Print a header at the top of the new file
+//  printHeader(); // Print a header at the top of the new file
+  File logFile = SD.open(logFileName, FILE_WRITE);
+  logFile.println(COLUMN_NAMES);
+  logFile.close();
+  
 }
 
 void loop()
@@ -149,26 +154,26 @@ byte logGPSData()
     logFile.print(comma);
     logFile.print(tinyGPS.satellites.value());
     logFile.print(comma);
-    AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-    AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-    logFile.print(AcX);
+//    AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+//    AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+//    AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+//    Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+//    GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+//    GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+//    GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+    logFile.print(Wire.read()<<8|Wire.read());
     logFile.print(comma);
-    logFile.print(AcY);
+    logFile.print(Wire.read()<<8|Wire.read());
     logFile.print(comma);
-    logFile.print(AcZ);
+    logFile.print(Wire.read()<<8|Wire.read());
     logFile.print(comma);
-    logFile.print(Tmp/340.00+36.5);
+    logFile.print((Wire.read()<<8|Wire.read())/340.00+36.5);
     logFile.print(comma);
-    logFile.print(GyX);
+    logFile.print(Wire.read()<<8|Wire.read());
     logFile.print(comma);
-    logFile.print(GyY);
+    logFile.print(Wire.read()<<8|Wire.read());
     logFile.print(comma);
-    logFile.print(GyZ);
+    logFile.print(Wire.read()<<8|Wire.read());
     logFile.println();
     logFile.close();
 
@@ -179,24 +184,24 @@ byte logGPSData()
 }
 
 // printHeader() - prints our eight column names to the top of our log file
-void printHeader()
-{
-  File logFile = SD.open(logFileName, FILE_WRITE); // Open the log file
-  SerialMonitor.println(logFile);
-  if (logFile) // If the log file opened, print our column names to the file
-  {
-    int i = 0;
-    for (; i < LOG_COLUMN_COUNT; i++)
-    {
-      logFile.print(log_col_names[i]);
-      if (i < LOG_COLUMN_COUNT - 1) // If it's anything but the last column
-        logFile.print(','); // print a comma
-      else // If it's the last column
-        logFile.println(); // print a new line
-    }
-    logFile.close(); // close the file
-  }
-}
+//void printHeader()
+//{
+//  File logFile = SD.open(logFileName, FILE_WRITE); // Open the log file
+//  SerialMonitor.println(logFile);
+//  if (logFile) // If the log file opened, print our column names to the file
+//  {
+//    int i = 0;
+//    for (; i < LOG_COLUMN_COUNT; i++)
+//    {
+//      logFile.print(log_col_names[i]);
+//      if (i < LOG_COLUMN_COUNT - 1) // If it's anything but the last column
+//        logFile.print(','); // print a comma
+//      else // If it's the last column
+//        logFile.println(); // print a new line
+//    }
+//    logFile.close(); // close the file
+//  }
+//}
 
 // updateFileName() - Looks through the log files already present on a card,
 // and creates a new file with an incremented file index.
